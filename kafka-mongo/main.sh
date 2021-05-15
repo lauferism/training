d6566a5dc563b38aa6627efc31080105a910dc54
lauferism <lauferism@lauferism.localdomain>
2021-05-15 17:30:59 +0300

    fixed main.sh

--- kafka-mongo/main.sh
+++ kafka-mongo/main.sh
@@ -1,64 +1,53 @@
-# action=$1
-# region=$2
-# name=$3
-# docker_token=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
+action=$1
+name=$2
+region=$3
+github_token=$4
 
+deploy(){
+eksctl create cluster --name "${name}" --region "${region}"
 
-# # create cluster
-# eksctl create cluster --name ${name} --region ${region}
-# # connect to cluster 
-# aws eks update-kubeconfig --name ${name}
+aws eks update-kubeconfig --name "${name}"
 
-# # create secret
-# kubectl create secret docker-registry regcred --docker-server=docker.pkg.github.com --docker-username=lauferism --docker-password ${docker_token}
+# create secret
+kubectl create secret docker-registry regcred --docker-server=docker.pkg.github.com --docker-username=lauferism --docker-password ${github_token}
 
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install kafka bitnami/kafka
 
-# helm repo add bitnami https://charts.bitnami.com/bitnami
-# # installing kafka
-# helm install kafka bitnami/kafka
-
-# # installing mongodb
-# helm install mongodb bitnami/mongodb
+helm install mongodb bitnami/mongodb
 
 MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace default mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
 
 git_top_level=$(git rev-parse --show-toplevel)
 
-declare -A locations
-locations=(
-    [kafka_consumer]='api_server/kafka_consumer_container'
-    [api_server_continaer]='api_server/server_container'
-    [web_server]='web_server'
-    )
 
 deploy_api_server(){
-    cd ${git_top_level}/kafka-mongo/api_server/kafka_consumer_container
-    docker build --tag kafka_consumer .
-    docker tag kafka_consumer docker.pkg.github.com/lauferism/training/kafka_consumer:latest
-    docker push docker.pkg.github.com/lauferism/training/kafka_consumer:latest
-
-
-    cd ${git_top_level}/kafka-mongo/api_server/server_container
-    docker build --tag api_server_continaer .
-    docker tag api_server_continaer docker.pkg.github.com/lauferism/training/api_server_continaer:latest
-    docker push docker.pkg.github.com/lauferism/training/api_server_continaer:latest
-
-    cd ${git_top_level}/kafka-mongo/api_server
-
     sed "s/<MONGODB_ROOT_PASSWORD>/${MONGODB_ROOT_PASSWORD}/g" deployment.yaml
 
     kubectl apply -f .
 
 }
 
+
 deploy_web_Server(){
     cd ${git_top_level}/kafka-mongo/web_server
-    docker build --tag web_server .
-    docker tag web_server docker.pkg.github.com/lauferism/training/web_server:latest
-    docker push docker.pkg.github.com/lauferism/training/web_server:latest
 
     kubectl apply -f .
 
 
 }
 
+deploy_api_server
+deploy_web_Server
+
+}
+
+
+destroy(){
+	helm uninstall mongodb kafka
+	eksctl delete cluster --name "${name}" --region "${region}"
+
+}
+
+
+$action
