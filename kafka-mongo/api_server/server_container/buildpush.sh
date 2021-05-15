9df70a6fc290566f687d54f162fd6832319a1278
lauferism <lauferism@gmail.com>
2021-05-15 18:25:27 +0300

    Delete buildpush.sh

--- kafka-mongo/api_server/server_container/buildpush.sh
+++
@@ -1,5 +0,0 @@
-docker build --tag api_server_container .
-docker tag api_server_container docker.pkg.github.com/lauferism/training/api_server_container:latest
-docker push docker.pkg.github.com/lauferism/training/api_server_container:latest
-kubectl delete pod $(kubectl get pod | grep api-server | awk '{print $1}')
-kubectl get pod
